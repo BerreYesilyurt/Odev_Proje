b8cd4935a9ceeea2553c19250dd081e0d6889915
Berre Yesilyurt <berreyesilyurt23@gmail.com>
2023-12-14 13:59:33 +0300

    Bağlantı Adresinin Verilmesi:q

---
+++ EntityLayer/Concrete/Customer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Customer
+    {
+        [Key]
+        public int CustomerID { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Mail { get; set; }
+        public DateTime RegistrationDate { get; set; }
+        public string Gender { get; set; }
+        public bool Status { get; set; }
+
+    }
+}
