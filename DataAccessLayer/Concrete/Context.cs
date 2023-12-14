64f1288286dfb438219ceba9d6488438b87f7b6b
Berre Yesilyurt <berreyesilyurt23@gmail.com>
2023-12-14 14:04:45 +0300

    Tabloların Veri Tabanıyla İlişkilendirilmesi

--- DataAccessLayer/Concrete/Context.cs
+++ DataAccessLayer/Concrete/Context.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using EntityLayer.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,13 @@ using System.Threading.Tasks;
 
 namespace DataAccessLayer.Concrete
 {
-    internal class Context
+    public class Context:DbContext
     {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=(localdb)\\MSSQLLocalDB;database=Biletleme;integrated security=true")
+        }
+
+        public DbSet<Customer> Customers { get; set; }
     }
 }
